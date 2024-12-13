40d5bfa9a028722dfbaafc02da657629c3b6fc3a
kenny <kenny@users.noreply.github.com>
2024-12-14 01:15:27 +0800

    feat: aid null analysis for OptionalSyntax

--- src/CeeSharp.Core/Syntax/OptionalSyntax.cs
+++ src/CeeSharp.Core/Syntax/OptionalSyntax.cs
@@ -1,9 +1,13 @@
+using System.Diagnostics.CodeAnalysis;
+
 namespace CeeSharp.Core.Syntax;
 
 public readonly struct OptionalSyntax<TElement>(TElement? element)
     where TElement : SyntaxElement
 {
     public TElement? Element { get; } = element;
+    
+    [MemberNotNullWhen(true, nameof(Element))]
     public bool HasValue => Element != null;
 
     public static readonly OptionalSyntax<TElement> None = new();
