dff02885bc3fe4d97bc55deafc45b53363f6e775
kenny <kenny@users.noreply.github.com>
2024-12-15 07:08:23 +0800

    feat: add unimplemented declarations to DeclarationKind

--- src/CeeSharp.Core/Syntax/DeclarationKind.cs
+++ src/CeeSharp.Core/Syntax/DeclarationKind.cs
@@ -6,13 +6,19 @@ public enum DeclarationKind
     Namespace = 1 << 0,
     Class = 1 << 1,
     Struct = 1 << 2,
-    Enum = 1 << 3,
-    Method = 1 << 4,
+    Interface = 1 << 3,
+    Delegate = 1 << 4,
     Field = 1 << 5,
-    Constructor = 1 << 6,
-    EnumMember = 1 << 7,
-    ParameterList = 1 << 8,
-    AttributeList = 1 << 9,
+    Event = 1 << 6,
+    Enum = 1 << 7,
+    Method = 1 << 8,
+    Operator = 1 << 9,
+    Property = 1 << 10,
+    Indexer = 1 << 11,
+    Constructor = 1 << 12,
+    EnumMember = 1 << 13,
+    ParameterList = 1 << 14,
+    AttributeList = 1 << 15,
 
-    Type = Class | Struct | Enum
+    Type = Class | Struct | Enum | Interface
 }
